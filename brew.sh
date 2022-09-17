7fcae6f21ecf22414e1df1b48f7b1818928dee78
5unnyWind <72289221+5unnyWind@users.noreply.github.com>
2022-09-18 02:00:46 +0800

    Add files via upload

---
+++ brew.sh
@@ -0,0 +1,29 @@
+# for Apple Silicon Mac
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+/bin/bash brew-install/install.sh
+rm -rf brew-install
+test -r ~/.bash_profile && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
+test -r ~/.zprofile && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+brew update
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+for tap in core cask{,-fonts,-drivers,-versions} command-not-found; do
+    brew tap --custom-remote --force-auto-update "homebrew/${tap}" "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git"
+done
+brew update
+brew install --cask microsoft-edge
+brew install --cask baidunetdisk
+brew install node
+brew install --cask utools
+brew install --cask visual-studio-code
+brew install docker
+brew install docker-compose
+brew install termius
+brew install --cask qqmusic
+brew install --cask feishu
+brew install --cask tencent-lemon
+brew install --cask wechat
+brew install --cask start
