a46f042198175c699ab44d745ea2c242a41c920f
5unnyWind <72289221+5unnyWind@users.noreply.github.com>
2022-09-18 02:06:38 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,4 +1,5 @@
 # for Apple Silicon Mac
+xcode-select --install
 export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
